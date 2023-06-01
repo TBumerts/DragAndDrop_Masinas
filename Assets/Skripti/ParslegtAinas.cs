385c44a08a828d5faac0cd6cef4c957c6412dde1
Bumerts <bumertstomijs@gmail.com>
2023-06-01 10:40:50 +0300

    Sakotnes ekrana izveide un attelu tag pievienosana

---
+++ Assets/Skripti/ParslegtAinas.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ParslegtAinas : MonoBehaviour {
+    public void UzSakumu()
+    {
+        SceneManager.LoadScene("Starta", LoadSceneMode.Single);
+    }
+    //Pašiem jāuztaisa metode,kas pārslēdzno sākuma uz UI ainu
+    public void uzUI()
+    {
+        SceneManager.LoadScene("PilsetasAina", LoadSceneMode.Single);
+    }
+    public void Apturet()
+    {
+        Application.Quit();
+    }
+}
